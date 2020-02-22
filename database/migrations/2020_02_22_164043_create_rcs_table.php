422fa87063e9d6b6952e254c3799cc1c5cb03b5f
lg <lg52057@126.com>
2020-02-23 01:29:03 +0800

    修改数据库表文件

--- database/migrations/2020_02_22_164043_create_rcs_table.php
+++ database/migrations/2020_02_22_164043_create_rcs_table.php
@@ -16,6 +16,19 @@ class CreateRcsTable extends Migration
         Schema::create('rcs', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->timestamps();
+
+            $table->softDeletes();
+
+            $table->bigInteger('date',10)->default(0)->comment("时间");
+            $table->string('address',30)->default('')->comment("地点");
+            $table->string('who',50)->default('')->comment("人物");
+            $table->string('thing',50)->default('')->comment("干什么");
+            $table->string('project',20)->default('')->comment("项目");
+            $table->string('remark',60)->default('')->comment("备注");
+            $table->string('pic',100)->default('')->comment("图片");
+            $table->bigInteger('uptime',10)->default(0)->comment("修改时间");
+            $table->tinyInteger('is_enabled',1)->default(1)->comment("是否正确(0:否,1:是)");
+
         });
     }
 
